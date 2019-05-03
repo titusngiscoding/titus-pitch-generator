a6c37a985439cb356ea7674811a21f8caf9a471a
Titus Ng <titusngiscoding@gmail.com>
2019-05-03 21:51:35 +0800

    followed most of pep8 style

--- titus-pitch-generator.py
+++ titus-pitch-generator.py
@@ -1,19 +1,23 @@
-import os, time
+import os
+import time
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 from pygame import mixer
-from validation import *
+from validation import sanitizeInput, caseInsensitiveIn, caseInsensitiveIndex
 from pitch_generator import PitchGenerator
 
-MUSIC_NOTES=["C","C#","Db","D","D#","Eb","E","F","F#","Gb","G","G#","Ab","A","A#","Bb","B"]
-YES_RESPONSES=["y","yes"]
-NO_RESPONSES=["n","no"]
+MUSIC_NOTES = ["C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G", "G#", "Ab", "A", "A#", "Bb", "B"]
+YES_RESPONSES = ["y", "yes"]
+NO_RESPONSES = ["n", "no"]
 
-def generateFilename(note,standard):
-    return "{}4({}).wav".format(note,standard)
+
+def generateFilename(note, standard):
+    return "{}4({}).wav".format(note, standard)
+
 
 def getIsTryAgain():
-    tryagain = sanitizeInput(YES_RESPONSES + NO_RESPONSES,"One more time? (y/n): ")
-    return caseInsensitiveIn(tryagain,YES_RESPONSES)
+    tryagain = sanitizeInput(YES_RESPONSES + NO_RESPONSES, "One more time? (y/n): ")
+    return caseInsensitiveIn(tryagain, YES_RESPONSES)
+
 
 def playNote(filepath):
     mixer.init()
@@ -23,21 +27,23 @@ def playNote(filepath):
     player.play()
     time.sleep(length)
 
+
 def main():
     print("titus-pitch-generator 1.0")
-    standard=int(sanitizeInput([str(x) for x in range(400, 500)],"Please enter standard (e.g. 440, 432): "))
-    pg=PitchGenerator(standard=standard)
+    standard = int(sanitizeInput([str(x) for x in range(400, 500)], "Please enter standard (e.g. 440, 432): "))
+    pg = PitchGenerator(standard=standard)
     print("Notes available: {}".format(MUSIC_NOTES))
     print()
-    again=True
+    again = True
     while again:
-        note = sanitizeInput(MUSIC_NOTES,"Please enter a note: ")
-        note = MUSIC_NOTES[caseInsensitiveIndex(note,MUSIC_NOTES)]
-        filepath = pg.generateWAV(note,generateFilename(note,standard))
+        note = sanitizeInput(MUSIC_NOTES, "Please enter a note: ")
+        note = MUSIC_NOTES[caseInsensitiveIndex(note, MUSIC_NOTES)]
+        filepath = pg.generateWAV(note, generateFilename(note, standard))
         playNote(filepath)
-        again=getIsTryAgain()
+        again = getIsTryAgain()
         print()
     input("Press Enter to exit ...")
-    
+
+
 if __name__ == "__main__":
     main()
--- validation.py
+++ validation.py
@@ -1,11 +1,13 @@
-def sanitizeInput(valid_responses,text):
+def sanitizeInput(valid_responses, text):
     response = ""
     while response.lower() not in [x.lower() for x in valid_responses]:
         response = input(text)
     return response
 
-def caseInsensitiveIndex(x,arr):
+
+def caseInsensitiveIndex(x, arr):
     return ([i.lower() for i in arr].index(x.lower()))
 
-def caseInsensitiveIn(x,arr):
+
+def caseInsensitiveIn(x, arr):
     return (x.lower() in [i.lower() for i in arr])
