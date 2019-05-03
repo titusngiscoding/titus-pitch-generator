678a639ea69b914f1d2ef0e62d049c895ac516a7
Titus Ng <titusngiscoding@gmail.com>
2019-05-03 11:05:14 +0800

    Did 2 things -- see description
    1) moved playNote function from pitch_gnerator to titus-pitch-generator
    2) added "try again" option

--- titus-pitch-generator.py
+++ titus-pitch-generator.py
@@ -1,13 +1,41 @@
-from pitchgen import PitchGenerator
+import os, time
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+from pygame import mixer
+from pitch_generator import PitchGenerator
 
 MUSIC_NOTES=["C","C#","Db","D","D#","Eb","E","F","F#","Gb","G","G#","Ab","A","A#","Bb","B"]
+
+def playNote(filename):
+    mixer.init()
+    player = mixer.Sound(filename)
+    length = player.get_length()
+    print("Playing {} ...".format(filename))
+    player.play()
+    time.sleep(length)
+
+def getIsTryAgain():
+    while(True):
+        inputDecision = input("One more time? (y/n): ").lower()
+        if inputDecision in ["y","yes"]:
+            return True
+        elif inputDecision in ["n","no"]:
+            return False
+
 def main():
     print("titus-pitch-generator 1.0")
     print("Notes available: {}".format(MUSIC_NOTES))
-    note = input("Enter note: ")
-    pg = PitchGenerator()
-    filename = pg.generateWAV(note)
-    pg.playNote(filename)
+    again=True
+    while again:
+        print()
+        note = input("Please enter note: ")
+        standard=440
+        print("Asserting A4 = 440 hz")
+        filename="cache/{}4({}).wav".format(note,standard)
+        if not os.path.exists(filename):
+            pg = PitchGenerator(standard=standard)
+            pg.generateWAV(note,filename)
+        playNote(filename)
+        again=getIsTryAgain()
     input("Press Enter to exit ...")
     
 if __name__ == "__main__":
